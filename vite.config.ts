c2712c78c30562ce7c66563b671af348496fc6e0
Mikhail Ruzin <rzn@feip.dev>
2025-10-14 23:44:29 +0300

    Added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,50 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import process from 'node:process';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                skipWaiting: true,
+            },
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'wihaven',
+                short_name: 'wihaven',
+                description: 'Tool to distribute income between expenses',
+                theme_color: '#242424',
+                icons: [
+                    {
+                        src: '/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any',
+                    },
+                    {
+                        src: '/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                ],
+            },
+        }),
+        react(),
+    ],
     css: {
         preprocessorOptions: {
             scss: {
